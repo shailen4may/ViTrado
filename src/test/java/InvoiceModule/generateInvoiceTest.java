644fc22de6ab2c5b40144b8a19d65cf65b5f905b
Shailendra <Shailendra@DESKTOP-0U7543P>
2024-01-24 22:58:21 +0530

    trade

---
+++ src/test/java/InvoiceModule/generateInvoiceTest.java
@@ -0,0 +1,13 @@
+package InvoiceModule;
+
+import org.testng.annotations.Test;
+
+public class generateInvoiceTest
+{
+		@Test
+		public void invoiceTest()
+		{
+			System.out.println("Invoice success");
+		}
+
+}
