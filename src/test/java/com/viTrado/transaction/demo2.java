60cb742f984e4723ab5a78bcbd2cda7071e9349a
Shailendra <Shailendra@DESKTOP-0U7543P>
2024-01-25 11:56:43 +0530

    commit last of the dai

---
+++ src/test/java/com/viTrado/transaction/demo2.java
@@ -0,0 +1,14 @@
+package com.viTrado.transaction;
+
+import org.testng.annotations.Test;
+
+public class demo2
+{
+	@Test
+	public void demo2Test()
+	{
+		System.out.println("Demo 2 ");
+		System.out.println("approve 2");
+	}
+
+}
---
+++ src/test/java/com/viTrado/transaction/demo3.java
@@ -0,0 +1,14 @@
+package com.viTrado.transaction;
+
+import org.testng.annotations.Test;
+
+public class demo3 
+{
+	@Test
+	public void demo3Test()
+	{
+		System.out.println("Demo 3");
+		System.out.println("approve 3");
+	}
+
+}
