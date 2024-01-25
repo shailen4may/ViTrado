2277e7c6a03adde0461d651b58eead3b73ef4ec1
My PC <My PC@DESKTOP-T56DNVO>
2024-01-25 12:12:59 +0530

    demo4

---
+++ src/test/java/com/viTrado/transaction/Demo4Test.java
@@ -0,0 +1,12 @@
+package com.viTrado.transaction;
+
+import org.testng.annotations.Test;
+
+public class Demo4Test {
+	@Test
+	public void demoTest()
+	{
+		System.out.println("Demo4");
+		System.out.println("approve4");
+	}
+}
